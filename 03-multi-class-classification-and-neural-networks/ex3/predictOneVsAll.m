dae73fb492f9e278410e80d52b9311f8c718201d
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-03-03 22:59:09 -0300

    implement one vs all classifier

--- 03-multi-class-classification-and-neural-networks/ex3/predictOneVsAll.m
+++ 03-multi-class-classification-and-neural-networks/ex3/predictOneVsAll.m
@@ -28,13 +28,10 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%
 
-
-
-
-
-
+H = sigmoid(X * all_theta');
+[max_value, p] = max(H, [], 2);
 
 % =========================================================================
 
