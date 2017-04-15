ba3c651a76ad63922c257dee614a04170878a3d6
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-04-15 16:41:47 -0300

    implement compute centroids method

--- 07-k-means-clustering-and-pca/ex7/computeCentroids.m
+++ 07-k-means-clustering-and-pca/ex7/computeCentroids.m
@@ -26,12 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
-
-
-
-
+for k = 1:K
+	id = find(idx == k)
+	centroids(k, :) = sum(X(id, :)) / size(id, 1)
+end
 
 % =============================================================
 
